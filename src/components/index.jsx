214b8677a292c178ea1fe42e85156a84d0049bd6
李刘胜 <542367736@qq.com>
2021-04-14 21:23:43 +0800

    feat():新增地图选择组件

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,10 +2,12 @@ import CommonNavBar from './CommonNavBar/index';  // 头部导航
 import ContainerWithCorner from './ContainerWithCorner';  // 外部容器
 import ContainerWithBorder from './ContainerWithBorder';  // 内部容器
 import GridLayout from './GridLayout'; // grid 布局容器
+import CommomMap from './CommonMap'; // 公共地图样式
 
 export {
   CommonNavBar,
   ContainerWithCorner,
   ContainerWithBorder,
   GridLayout,
+  CommomMap
 }
--- src/pages/ServiceAgencies/index.jsx
+++ src/pages/ServiceAgencies/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-import { CommonNavBar, ContainerWithCorner } from '../../components/index'
 import './index.scss'
 import { useHistory } from "react-router-dom";
+import { CommonNavBar, ContainerWithCorner, GridLayout, ContainerWithBorder, CommomMap } from '../../components';
+import './index.scss';
 
+// 布局数据
+const layout = [
+  { i: '0', x: 0, y: 0, w: 6, h: 24 }, // 左侧
+  { i: '1', x: 6, y: 0, w: 12, h: 16 }, // 地图
+  { i: '2', x: 6, y: 16, w: 12, h: 8 }, // 中下
+  { i: '3', x: 18, y: 0, w: 6, h: 8 }, // 右上
+  { i: '4', x: 18, y: 8, w: 6, h: 8 }, // 右中
+  { i: '5', x: 18, y: 16, w: 6, h: 8 }, // 右下
+];
 
 const ServiceAgencies = () => {
   const history = useHistory();
@@ -11,6 +21,16 @@ const ServiceAgencies = () => {
     <div className="service-agencies">
       <CommonNavBar showTime={true} title="服务机构" btnType="back"></CommonNavBar>
       <ContainerWithCorner className="service-agencies-content">
+        <GridLayout layout={layout}>
+          <ContainerWithBorder key="0" className="grid-item-content"></ContainerWithBorder>
+          <ContainerWithBorder key="1" className="grid-item-content">
+            <CommomMap callBack={() => { }}></CommomMap>
+          </ContainerWithBorder>
+          <ContainerWithBorder key="2" className="grid-item-content"></ContainerWithBorder>
+          <ContainerWithBorder key="3" className="grid-item-content"></ContainerWithBorder>
+          <ContainerWithBorder key="4" className="grid-item-content"></ContainerWithBorder>
+          <ContainerWithBorder key="5" className="grid-item-content"></ContainerWithBorder>
+        </GridLayout>
       </ContainerWithCorner>
     </div >
   )
